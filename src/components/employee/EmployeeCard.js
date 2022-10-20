7bc226f7f01727df8c48a6b5ea24fc235f273cf3
Ashan Tharindu <ashantharindu@192.168.1.3>
2022-10-20 22:01:42 +0530

    feat: add edit and delete button

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -19,6 +20,12 @@ const EmployeeCard = ({ name, email, address, role }) => {
         <Typography variant='body2'>
           {address}
         </Typography>
+        <Button variant="contained" color="success">
+        Edit
+      </Button>
+      <Button variant="contained" color="error">
+        Delete
+      </Button>
       </CardContent>
 
     </Card>
