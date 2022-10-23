a0fea30b342cb44865988b32eec88983b69c138d
Ashan Tharindu <ashantharindu@Ashans-MacBook-Pro.local>
2022-10-23 17:27:33 +0530

    fix: base url fixed

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'http://0.0.0.0:3001/employee-management',
+  baseURL: 'https://whispering-castle-21078.herokuapp.com/employee-management',
   timeout: 1000 * 5,
   headers: {
     'Content-Type': 'application/json',
